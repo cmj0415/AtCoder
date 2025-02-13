c8269711436e5514c166381fb3432a8207cd54a5
cmj0415 <cmj51406002@gmail.com>
2025-02-13 16:42:47 +0800

    Problem E almost solved (21 AC and 1 WA)

--- ABC392/ABC392_E.cpp
+++ ABC392/ABC392_E.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <vector>
-#define ff first
-#define ss second
 using namespace std;
+typedef struct {
+    int op;
+    pair<int, int> pr;
+} Edge;
 int find(int x, vector<int> &parent) {
-    return x == parent[x] ? x : find(parent[x], parent);
+    if (parent[x] != x) {
+        parent[x] = find(parent[x], parent);
+    }
+    return parent[x];
 }
 int Union(int x, int y, vector<int> &parent) {
     int r1 = find(x, parent);
@@ -19,70 +24,54 @@ int Union(int x, int y, vector<int> &parent) {
 int main() {
     int n, m;
     cin >> n >> m;
-    vector<int> adj[n + 1];
 
-    // DSU
+    // parent array for DSU
     vector<int> parent(n + 1);
     for (int i = 1; i <= n; i++) parent[i] = i;
-    
+
     // construct graph and find loops
-    vector<pair<int, int>> vec; // records the edges that cause loops
+    vector<Edge> vec; // records the edges that cause loops
+    vector<bool> exist(n + 1, 0);
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        exist[u] = exist[v] = 1;
         if (Union(u, v, parent)) {
-            printf("Loop occurs when connecting %d %d\n", u, v);
-            vec.push_back({u, v});
+            vec.push_back({i + 1, {u, v}});
         }
     }
-    // calculate the number of connected components
-    int components = 0;
-    vector<bool> vis(m + 1, 0);
-    vector<int> queue(m);
-    vector<int> code(n + 1); // records which component each vertex is in
-    vector<int> representative(n + 1); // records a representative vertex of a component
-    // bfs
-    int idx = 1;
+
+    // count the number of connected components (which is the number of roots)
+    int count = 0;
     for (int i = 1; i <= n; i++) {
-        if (vis[i]) continue;
-        int top = 0, rear = 0;
-        representative[idx] = i;
-        queue[rear++] = i;
-        vis[i] = true;
-        while(top < rear){
-            int pos = queue[top++];
-            code[pos] = idx; // vertex 'pos' is in the idx-th component
-            for (auto j : adj[pos]) {
-                if (vis[j]) continue;
-                queue[rear++] = j;
-                vis[j] = true;
-            }
+        if (parent[i] == i) {
+            count++;
         }
-        components++;
-        idx++;
     }
-    for (int i : representative) printf("%d ", i);
-    printf("\n");
-    printf("%d\n", components - 1);
-    if (components == 1) return 0;
+    
+    printf("%d\n", count - 1);
 
-    // record codes of components with no loop containing
-    vector<bool> hasLoop(components + 1, 0);
-    for (auto p : vec) {
-        hasLoop[code[p.first]] = 1;
-        hasLoop[code[p.second]] = 1;
-    }
-    vector<int> noLoopComponents;
-    for (int i : hasLoop) {
-        if (!hasLoop[i]) noLoopComponents.push_back(i);
+    // first pass: connecting lone vertices
+    int index = 0;
+    for (int i = 1; i <= n; i++) {
+        if (exist[i]) continue;
+        Union(vec[index].pr.first, i, parent);
+        printf("%d %d %d\n", vec[index].op, vec[index].pr.first, i);
+        index++;
     }
 
-    // reconnect redundant edges to another component
-    int index = 0, size = vec.size();
-    for (auto p : vec) {
-        printf("%d %d %d", p.first, p.second, representative[noLoopComponents[index]]);
-        if (index < vec.size() - 1) index++;
+    // second pass: connecting each components with vertex 1
+    for (int i = 2; i <= n; i++) {
+        if (find(i, parent) == find(1, parent)) {
+            continue;
+        } 
+        if (find(vec[index].pr.first, parent) == find(1, parent)) {
+            Union(vec[index].pr.first, i, parent);
+            printf("%d %d %d\n", vec[index].op, vec[index].pr.first, i);
+        } else {
+            Union(1, vec[index].pr.first, parent);
+            printf("%d %d %d\n", vec[index].op, vec[index].pr.first, 1);
+        }
+        index++;
     }
 }
